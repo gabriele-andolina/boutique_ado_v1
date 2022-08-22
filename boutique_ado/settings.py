5a11a6e7db51c3b73a66695732d77d836dbaf70c
Gabriele Andolina <gabe.andolina@gmail.com>
2022-08-22 15:34:06 +0000

    Fix project name in wsgi.py, settings.py and Profcile

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'boutique_ado_v1.urls'
+ROOT_URLCONF = 'boutique_ado.urls'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
@@ -110,7 +110,7 @@ ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
-WSGI_APPLICATION = 'boutique_ado_v1.wsgi.application'
+WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 
 # Database
